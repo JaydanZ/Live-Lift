5feb707766c79654d898bd19c033e7f116b2c35e
Devin <71891263+devincar@users.noreply.github.com>
2022-04-02 15:34:27 -0400

    signup
    sing up page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import SignUp from "./components/SignUp/SignUp";
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
-  );
+      <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+        >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <SignUp />
+        </div>
+      </Container>
+  )
 }
 
 export default App;
