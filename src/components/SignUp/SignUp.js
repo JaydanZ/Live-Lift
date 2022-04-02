36c323f3c7cf897fe71ca36ae4f2c340270dacc7
Taimoors22 <tsalam7246@conestogac.onca>
2022-04-02 15:10:13 -0400

    first commit

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import "./SignUp.css"
+import {Form, Button Card} from 'react-bootstrap'
+import React from "react";
+
+const SignUp = () =>{
+
+    return(
+        <React.Fragment>
+        </ React.Fragment>
+    );
+}
+
+export default SignUp;
